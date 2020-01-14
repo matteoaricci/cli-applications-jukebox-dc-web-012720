9db5405542705cf9887ba2b7fb778e40b6ce7652
Matteo Ricci <matteo.a.ricci@gmail.com>
2020-01-14 13:27:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,15 @@
 # Add your code here
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def play
+  puts "Please enter a song name or number: "
+  song_input = gets.strip
+  
+  
+end
